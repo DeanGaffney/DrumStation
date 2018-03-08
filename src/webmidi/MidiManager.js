fc830bcbdff2e947c98df8092d0478cd8140316d
Dean <deanpatrickgaffney@gmail.com>
2018-03-08 18:36:00 +0000

    added enable method in MidiManager

--- src/webmidi/MidiManager.js
+++ src/webmidi/MidiManager.js
@@ -2,8 +2,22 @@
 import WebMidi from '../../node_modules/webmidi/webmidi.min.js'
 
 class MidiManager {
+    
     constructor(){
-        
+        this.enable();
+    }
+
+    /**
+     * Enables WebMidi
+     */
+    enable(){
+        WebMidi.enable(function (err) {
+            if (err) {
+                console.log("WebMidi could not be enabled.", err);
+            } else {
+                console.log("WebMidi enabled!");
+            }
+        });
     }
 }
 
