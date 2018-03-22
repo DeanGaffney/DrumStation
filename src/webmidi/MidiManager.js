18caad58fa059bac84122ee48086bd6c2fa29dda
conorcorcoran <conorcorcoran96@gmail.com>
2018-03-22 23:48:17 +0000

    Got a one bar loop working, stuck in loop

--- src/webmidi/MidiManager.js
+++ src/webmidi/MidiManager.js
@@ -24,8 +24,9 @@ class MidiManager {
         });
     }
 
-    playNote(note, channel,time){
+    playNote(note, channel,time, dur){
       midiManager.output.playNote(note, channel, {time: time});
+      midiManager.output.stopNote(note, channel, {time: time + dur});
     }
 
     drumLoop(bpm, isPlaying, drum){
@@ -33,7 +34,7 @@ class MidiManager {
       if(isPlaying){
         for(var i = 0; i<drum.steps;i++){
           var offset = (i * 16 * sixteenth) - sixteenth;
-          midiManager.playNote(drum.note, "all", sixteenth*drum.steps[i]+offset);
+          midiManager.playNote(drum.note, "all", sixteenth*drum.steps[i]+offset, sixteenth);
         }
       }
     }
@@ -46,12 +47,24 @@ class MidiManager {
      * Plays the drums
      * @param {Object} appState - the entire App.js state
      */
-    play(appState){
+    play(appState, drums){
         console.log(appState);
-        // midiManager.intervalId = setInterval(midiManager.playNote, 1000, "D1", "all", 1000);//Referrence to the fucnction, interval time, note, channel, duration
-        midiManager.intervalId = setInterval(() => {
-          midiManager.playNote("D1", "all", 1000);
-        }, 1000);
+        var repeat = 16;
+        drums = [appState.bass, appState.snare, appState.hihat];
+        for(var k = 0; k < repeat; repeat++){
+          for(var i = 0; i < drums.length; i++){
+            for(var j = 0; i < drums[i].steps.length; j++){
+              midiManager.playNote(drums[i].note, "all");
+            }
+          }
+        }
+        var sixteenth = (60/appState.bpm /4 ) * 1000;
+        midiManager.playOneBarLoop(sixteenth);
+      //   // midiManager.intervalId = setInterval(midiManager.playNote, 1000, "D1", "all", 1000);//Referrence to the fucnction, interval time, note, channel, duration
+      // midiManager.intervalId = setInterval(() => {
+      // midiManager.drumLoop(appState.bpm, appState.isPlaying, appState.bass)
+      //
+      // },60/appState.bpm/4*1000);
     }
 
     /**
@@ -59,8 +72,29 @@ class MidiManager {
      */
     stop(){
         clearInterval(midiManager.intervalId);      //stop the setInterval running
+        this.output.sendStop();
         console.log("Not Playing!");
     }
+
+    playOneBarLoop(sixteenth){
+      for (var i=0; i<4; i++){
+
+        var offset = (i * 16 * sixteenth) - sixteenth;
+
+        //       1234567890123456
+        //KD:36: X   X   X   X
+        midiManager.playNote("C1","all",sixteenth*1+offset,sixteenth);   // kd
+        midiManager.playNote("D1","all",sixteenth*5+offset,sixteenth);   // kd
+        midiManager.playNote("D1","all",sixteenth*13+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*1+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*2+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*3+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*5+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*9+offset,sixteenth);   // kd
+        midiManager.playNote("C1","all",sixteenth*13+offset,sixteenth);   // kd
+      }
+ }
+
 
 }
 
